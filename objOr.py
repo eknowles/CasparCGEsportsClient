3924da449cc4bd6372d0728aa7ad1af43d0069e1
Edward Knowles <ed@stunjelly.com>
2013-08-11 01:13:52 +0100

    nics help for objects

--- objOr.py
+++ objOr.py
@@ -1,17 +1,25 @@
 import random
+import math
+class Fish(object):
+	"""docstring for Fish"""
+	def __init__(self, name):
+		super(Fish, self).__init__()
+		self.name = name
+		self.price = 100
 
 class aquarium(object):
 	"""docstring for aquarium"""
-	def __init__(self, name):
+	def __init__(self, name, default_fish):
 		self.name = name
-		self.money = 100
-		self.default_fish = "trout"
+		self.money = 1000
+		self.default_fish = default_fish
 		self.fill_fishtank()
 
 	def fill_fishtank (self):
-		for i in range(0,random.random()):
+		self.fishtank = []
+		for i in range(0,math.floor(random.uniform(1,10))):
 			self.fishtank.append(self.default_fish())
-
+			self.money = self.money - self.default_fish.price
 
 	def sell_fish(self, fish):
 		if fish < len(self.fishtank):
@@ -19,15 +27,20 @@ class aquarium(object):
 			self.money = self.money + 100
 
 class sharkAquarium(aquarium):
-	def __init__(self, name, dangerlevel):
+	def __init__(self, name, default_fish, dangerlevel):
 		aquarium.__init__(self, name)
 		self.dangerlevel = dangerlevel
-		self.default_fish = "GREAT MOTHER FUCKING WHITE"
+		self.money = 10000
+		self.default_fish = default_fish
+		self.fill_fishtank()
      
 def main():
-	joes = aquarium('Joes Fish Tank')
-	franks = aquarium('Franks Diving Bell')
-	bills = sharkAquarium('Bills Jaws', 'VERY FUCKING HIGH')
+	standard_fish = Fish('trout')
+	fucking_horrbile_fish = Fish('HAMMERTIME')
+	fucking_horrbile_fish.price = 1000
+	joes = aquarium('Joes Fish Tank', standard_fish)
+	franks = aquarium('Franks Diving Bell', standard_fish)
+	bills = sharkAquarium('Bills Jaws', 'VERY FUCKING HIGH', fucking_horrbile_fish)
 	joes.sell_fish(1)
 	bills.
 	print 'Joes:', joes.__dict__
